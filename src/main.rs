219176ee37f4a1927321a8ea4914a6c10fa1b351
Patrick O'Doherty <p@trickod.com>
2015-10-03 18:18:45 -0700

    Rename to wuphf

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate log;
 extern crate env_logger;
-extern crate silent_doorbell;
+extern crate wuphf;
 extern crate request;
 
 use std::env;
@@ -10,10 +10,10 @@ fn main() {
     env_logger::init().unwrap();
 
     let user_file = env::args().nth(1).unwrap();
-    let users = silent_doorbell::users::read_users(user_file).unwrap();
-    let patrick: silent_doorbell::users::User = users[0].clone();
+    let users = wuphf::users::read_users(user_file).unwrap();
+    let patrick: wuphf::users::User = users[0].clone();
 
-    match silent_doorbell::sms::send_sms("+15005550006", &patrick.phone_number.unwrap(), "Hey there") {
+    match wuphf::sms::send_sms("+15005550006", &patrick.phone_number.unwrap(), "Hey there") {
         Ok(message_sid) => { info!("Sent SMS: {}", message_sid); },
         Err(err) => { panic!("Error: {:?}", err) }
     }
